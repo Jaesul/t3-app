5af53bb19ae7faecc2f18008fbf5ffe845ca2ca5
jaesukBT <168682184+jaesukBT@users.noreply.github.com>
2024-05-17 14:59:21 -0700

    added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,45 @@ import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+
+export const dynamic = "force-dynamic";
+
+async function Images() {
+  const image = await db.query.image.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
+  return (
+    <div className="flex flex-wrap">
+
+      {
+        image.map((image) => (
+          <div key={image.id} className="w-1/2 p-4 flex flex-col">
+            <img src={image.url} alt="image" />
+            <div>
+              {image.name}
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
 
 export default async function Home() {
 
-  const image = await db.query.image.findMany({
-    orderBy: (model, {desc}) => desc(model.id)
-  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="flex flex-wrap">
-
-        {
-          image.map((image) => (
-            <div key={image.id} className="w-1/2 p-4 flex flex-col">
-                <img src={image.url} alt="image" />
-                <div>
-                  {image.name}
-                </div>
-            </div>
-          ))
-        }
-      </div>
-      </main>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
+    </main>
   );
 }
 
